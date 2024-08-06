402745b04c718bc7048bec22bcbdcac7d84de545
Denis <ddziubek.11@gmail.com>
2024-08-06 02:13:34 +0200

    added pagination handlers and fetching data

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,24 @@
-export default function PokemonCard(p) {
+export default function PokemonCard({image, name, types=[]}) {
   return (
     <>
       <div className="card bg-neutral text-neutral-content">
         <figure className="p-2">
           <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
-            alt="pokemon name"
+            src={image}
+            alt={name}
           />
         </figure>
 
         <div className="card-body items-center text-center">
           <div className="flex flex-row gap-x-2">
-            <div className="badge badge-primary">ability 1</div>
-            <div className="badge badge-secondary">ability 2</div>
-            <div className="badge badge-accent">ability 3</div>
+            {types.map((type, index) => (
+              <div key={index} className={`badge badge-${index % 3 === 0 ? 'primary' : index % 3 === 1 ? 'secondary' : 'accent' }`}>
+                {type}
+                </div>
+            ))}
+            
           </div>
-          <h2 className="card-title">Pokemon name</h2>
+          <h2 className="card-title">{name}</h2>
           <p>We are using cookies for no reason.</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Accept</button>
