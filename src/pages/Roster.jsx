83716a4cf222965a0661e31c00aa8b2f48f6f732
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-08-09 11:58:57 +0200

    Fixed function handleSave to avoid more that one pokemon with same id

--- src/pages/Roster.jsx
+++ src/pages/Roster.jsx
@@ -5,7 +5,7 @@ import Skeleton from "../components/Skeleton";
 
 export default function Roster() {
   const [roster, setRoster] = useState([]);
- 
+
   const navigate = useNavigate();
   useEffect(() => {
     const myPoke = JSON.parse(localStorage.getItem("myPokemon"));
@@ -14,10 +14,9 @@ export default function Roster() {
     }
   }, []);
 
-
   const handleBattleClick = (p) => {
     const selectedPokemon = { id: p.id };
-    navigate('/battle', { state: { selectedPokemon } });
+    navigate("/battle", { state: { selectedPokemon } });
   };
 
   return (
@@ -29,51 +28,62 @@ export default function Roster() {
 
         <div className="container m-auto grid grid-cols-4 gap-4 my-10">
           {roster.length > 0 ? (
-            roster.map((p) => (
-              <div
-                key={p.name}
-                className="card card-compact bg-neutral text-neutral-content"
-              >
-                <figure className="p-2">
-                  <img src={p.image} alt={p.name} />
-                </figure>
-                <div className="card-body items-center text-center">
-                  <h2 className="card-title capitalize">{p.name}</h2>
-                  <div className="card-actions justify-end">
-                    
-                      <button  onClick={()=> handleBattleClick(p)} className="btn btn-sm btn-secondary">
+            roster.map((p) => {
+              const handleRemove = (e, idToRemove) => {
+                e.preventDefault();
+                idToRemove = p.id;
+                console.log(`Function handle remove ${idToRemove}`);
+              };
+              return (
+                <div
+                  key={p.name}
+                  className="card card-compact bg-neutral text-neutral-content"
+                >
+                  <figure className="p-2">
+                    <img src={p.image} alt={p.name} />
+                  </figure>
+                  <div className="card-body items-center text-center">
+                    <h2 className="card-title capitalize">{p.name}</h2>
+                    <div className="card-actions justify-end">
+                      <button
+                        onClick={() => handleBattleClick(p)}
+                        className="btn btn-sm btn-secondary"
+                      >
                         Battle
                       </button>
-                  
-                    <Link to={`/pokemon/${p.name}`}>
-                      <button className="btn btn-sm btn-accent">More</button>
-                    </Link>{" "}
-                    <button
-                      className="btn btn-sm btn-default"
-                      // onClick={handleRemove}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
+                      <Link to={`/pokemon/${p.name}`}>
+                        <button className="btn btn-sm btn-accent">More</button>
+                      </Link>
+                      <button
+                        className="btn btn-sm btn-default"
+                        onClick={handleRemove}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
+              );
+            })
           ) : (
             <>
-              <Skeleton />
+              <p className="text-lg font-bold">
+                You have not caught any pokemon yet
+              </p>
+
               <Skeleton />
               <Skeleton />
               <Skeleton />
